d2f481455bd5e823d51db4ee0274ebe45d47d181
Vasile Cosmin <vcosmin961@gmail.com>
2022-09-20 13:07:28 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const idContainer = document.querySelector('span')
+const adviceContainer = document.querySelector('#advice')
+const button =  document.querySelector('#dice')
+
+const url = `https://api.adviceslip.com/advice`
+
+button.addEventListener('click', () => {
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    let id = data.slip.id
+    let advice = data.slip.advice
+
+    idContainer.textContent = `#${id}`
+    adviceContainer.textContent = `"${advice}"`
+
+    console.log(id)
+    console.log(advice)
+  })
+  .catch(err => console.error(err))
+})
+
+// button.addEventListener('click', getAdvice)
+
+// async function getAdvice() {
+//   let response = await fetch(url)
+//   let data = await response.json()
+
+//   let id = data.slip.id
+//   let advice = data.slip.advice
+
+//   console.log(id)
+//   console.log(advice)
+
+//   idContainer.textContent = `#${id}`
+//   adviceContainer.textContent = `"${advice}"`
+// }
